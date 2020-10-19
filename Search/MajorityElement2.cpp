3a1c7df600112b6ee6c90a1ab74157938c62c165
VaishnaviSrivastavaCodes <72790971+VaishnaviSrivastavaCodes@users.noreply.github.com>
2020-10-19 13:03:31 +0530

    Create MajorityElement2.cpp
    https://leetcode.com/problems/majority-element-ii/
    O(n) space and O(n) time

---
+++ Search/MajorityElement2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+         int n= nums.size();int i; vector<int> v;
+        map<int,int>  mp;  
+        for(i=0;i<n;i++){
+            mp[nums[i]]++; // count of each element
+                 }
+        for(int i=0;i<n;i++){ // check if count>n/3
+            if(mp[nums[i]]>n/3  && std::find(v.begin(),v.end(),nums[i])==v.end())
+                v.push_back(nums[i]);
+        }
+        return v;
+    }
+};
