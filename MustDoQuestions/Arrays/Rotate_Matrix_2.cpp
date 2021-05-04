9c2c4adaafe29956e5d074c13086f6a94a7d0de2
Vaishnavi Srivastava <72790971+VaishnaviSrivastavaCodes@users.noreply.github.com>
2021-05-04 18:28:25 +0530

    Create Rotate_Matrix_2.cpp
    O(1) Space, O(m*n) time

---
+++ MustDoQuestions/Arrays/Rotate_Matrix_2.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/rotate-image/
+// No extra space used
+// Reverse column, then transpose
+//In case of clockwise, reverse row first using reverse(a[i], a[i] +n)
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        //reverse column
+        for(int i =0; i<n;i++){
+            int sr =0, er = n-1;
+            
+            while(sr<er){
+                swap(matrix[sr][i], matrix[er][i]);
+                sr++; er--;
+            }
+        }
+         //Transpose        
+        for(int i =0; i<n;i++){
+            for(int j =0 ; j<n;j++ ){
+                if(i<j){
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+        }
+    }
+};
